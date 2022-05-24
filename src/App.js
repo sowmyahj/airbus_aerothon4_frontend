a753bbc70c0e4bffce1303cc2888f7dfc6438397
Aditya <adityasatya100@gmail.com>
2022-05-24 21:25:47 +0530

    url correction

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   },[])
   return (
     <div className="App">
-     <a href="?fe=react&be=node&projName=react_node">Download React & Node</a>
+     <a href="http://localhost:1080?fe=react&be=node&projName=react_node">Download React & Node</a>
     </div>
   );
 }
