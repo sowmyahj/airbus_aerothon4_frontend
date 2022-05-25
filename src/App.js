3de0a6f0411b569c1eadb121918842c75eabf015
sowmya <sowmya.hj@gmail.com>
2022-05-25 16:26:41 +0530

    front end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect} from'react';
-import axios from 'axios';
-const beUrl = 'http://localhost:1080'
+import "./App.css";
+import { useState } from "react";
+
 function App() {
-  useEffect(()=>{
-  },[])
+  const [projectName, setProjectName] = useState("");
+  const [frontEnd, setFrontEnd] = useState("React");
+  const [backEnd, setBackEnd] = useState("Node");
+  const [db, setDb] = useState("MongoDB");
+
+  async function download(e) {
+    e.preventDefault();
+    const regex = /[^-A-Za-z0-9_]/;
+    if (
+      projectName.trim() === "" ||
+      projectName.length > 30 ||
+      regex.test(projectName)
+    ) {
+      alert("Please enter a valid project name.");
+      setProjectName("");
+      return;
+    }
+
+    let url =
+      "/api/downloadProject/" +
+      projectName +
+      "/" +
+      frontEnd +
+      "/" +
+      backEnd +
+      "/" +
+      db;
+    fetch(url);
+  }
+
   return (
-    <div className="App">
-     <a href="http://localhost:1080?fe=react&be=node&projName=react_node">Download React & Node</a>
+    <div>
+      <header className="App-header">
+        <h1> AIRBUS Aerothon 4.0 - Web application toolkit </h1>
+      </header>
+      <div className="App-form">
+        <form onSubmit={(e) => download(e)}>
+          <div className="mb-3 row">
+            <label className="col-sm-5 col-form-label fw-bold">
+              Enter project name{" "}
+            </label>
+            <input
+              className="w-25 form-control"
+              type="text"
+              placeholder="type here"
+              value={projectName}
+              onChange={(e) => setProjectName(e.target.value)}
+            />
+            <div className="form-text">
+              [Max 30 chars; Allowed chars: A-Z, a-z, 0-9, hyphen, underscore]
+            </div>
+          </div>
+          <br />
+          <div className="mb-3 row">
+            <label className="col-sm-5 col-form-label fw-bold">
+              {" "}
+              Select a Front-end framework
+            </label>
+            <select
+              className="w-25 form-select"
+              value={frontEnd}
+              onChange={(e) => setFrontEnd(e.target.value)}
+            >
+              <option value="React">React</option>
+              <option value="Angular">Angular</option>
+              <option value="Vue">Vue</option>
+              <option value="HTML">HTML</option>
+            </select>
+          </div>
+          <br />
+
+          <div className="mb-3 row">
+            <label className="col-sm-5 col-form-label fw-bold">
+              {" "}
+              Select a Back-end framework
+            </label>
+            <select
+              className="w-25 form-select"
+              value={backEnd}
+              onChange={(e) => setBackEnd(e.target.value)}
+            >
+              <option value="Node">Node(Express)</option>
+              <option value="SpringBoot">SpringBoot</option>
+              <option value="Django">Django</option>
+              <option value="Flask">Flask</option>
+            </select>
+          </div>
+          <br />
+          <div className="mb-3 row">
+            <label className="col-sm-5 col-form-label fw-bold">
+              Select a Database
+            </label>
+            <select
+              className="w-25 form-select"
+              value={db}
+              onChange={(e) => setDb(e.target.value)}
+            >
+              <option value="MongoDB">MongoDB</option>
+              <option value="MySql">MySql</option>
+            </select>
+          </div>
+          <br />
+          <br />
+          <button className="btn btn-primary">
+            {" "}
+            Download starter project{" "}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Airbus header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(
+    "AIRBUS Aerothon 4.0 - Web application toolkit"
+  );
+  expect(headerText).toBeInTheDocument();
 });
